66050358ce7d87a30d2b28198678519b296d1743
Brian <kds0281@gmail.com>
2023-10-29 18:04:49 -0700

    updated t5-model

--- inputters/__init__.py
+++ inputters/__init__.py
@@ -1,11 +1,12 @@
 
 from inputters.strat import Inputter as strat
 from inputters.vanilla import Inputter as vanilla
-
+from inputters.emp_dialog_t5 import Inputter as emp_dialog_t5
 
 inputters = {
     'vanilla': vanilla,
     'strat': strat,
+    'emp_dialog_t5' : emp_dialog_t5
 }
 
 
--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,12 @@
 
 from models.strat_blenderbot_small import Model as strat_blenderbot_small
 from models.vanilla_blenderbot_small import Model as vanilla_blenderbot_small
+from models.emp_dialog_t5 import Model as emp_dialog_t5
 
 
 models = {
-    
+
     'vanilla_blenderbot_small': vanilla_blenderbot_small,
     'strat_blenderbot_small': strat_blenderbot_small,
-    
+    'emp_dialog_t5': emp_dialog_t5,
 }
